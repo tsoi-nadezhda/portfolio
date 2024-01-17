f8623df7113d7d8518ee29129000748cc4b0adf4
NTsoi <czoj.n95@mail.ru>
2024-01-17 12:51:19 +0200

    28

--- src/index.js
+++ src/index.js
@@ -3,10 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+let dialogsData = [
+  { id: 1, name: "Tsoi" },
+  { id: 2, name: "Jek" },
+  { id: 3, name: "Lubava" },
+  { id: 4, name: "Verka" },
+  { id: 5, name: "Viktor" },
+]
+let messagesData = [
+  { id: 1, message: "Hi" },
+  { id: 2, message: "How are you?" },
+  { id: 3, message: "Thank you" },
+  { id: 4, message: "Bye" },
+  { id: 5, message: "Bye" },
+]
+let postData = [
+  { id: 1, message: "Hi! How are you?", likeCount: 15 },
+  { id: 2, message: "It's my first post", likeCount: 20 }
+]
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App dialogsData={dialogsData}  messagesData={messagesData} postData={postData}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
