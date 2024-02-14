83e6ac0be0d7275d580cad7feafdbd6a8254fba4
NTsoi <czoj.n95@mail.ru>
2024-02-14 10:59:06 +0200

    refactoring

--- src/components/Profile/MyPosts/MyPostsContainer.jsx
+++ src/components/Profile/MyPosts/MyPostsContainer.jsx
@@ -1,26 +1,7 @@
-import React from 'react'
-import s from "./MyPosts.module.css"
 import MyPosts from './MyPosts'
 import { addPostActionCreator, updatePostMessageCreator } from "../../../redux/profile_reducer"
-import StoreContext from "../../../StoreContext"
 import { connect } from 'react-redux'
-const MyPostsContainer1 = () => {
-  return (
-    <StoreContext.Consumer>
-      {store => {
-        const handleclick = () => {
-          store.dispatch(addPostActionCreator());
-        }
-        const onPostChange = (text) => {
-          store.dispatch(updatePostMessageCreator(text));
-        }
-        return (
-          <MyPosts handleclick={handleclick} onPostChange={onPostChange} newPostText={store.getState().profilePage.newPostText} postData={store.getState().profilePage.postData} />
-        )
-      }}
-    </StoreContext.Consumer>
-  )
-}
+
 const mapStateToProps = (state) => {
   return {
     newPostText: state.profilePage.newPostText,
